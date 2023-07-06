f98e86de73ae8f652d5df963aff765516527947c
8ark <sorlti6952@gmail.com>
2023-07-06 11:36:20 +0900

    FECH: layout
    + modified layout using {props.children}

--- meetup/src/components/meetups/MeetupItem.js
+++ meetup/src/components/meetups/MeetupItem.js
@@ -1,21 +1,26 @@
 import classes from "./MeetupItem.module.css";
 
+/*ui */
+import Card from "../ui/Card";
+
 function MeetupItem(props) {
   return (
-    <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
+    <Card>
+      <li className={classes.item}>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
 
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
-    </li>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </li>
+    </Card>
   );
 }
 
