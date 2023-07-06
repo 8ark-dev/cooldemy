472a858373682cfe4421ae1228fce719cd555840
8ark <sorlti6952@gmail.com>
2023-07-06 12:40:37 +0900

    FECH: backend
    + new data to firebase method POST

--- meetup/README.md
+++ meetup/README.md
@@ -4,3 +4,7 @@
 ### {props.children}
 children으로 컴포넌트의 모든 컨텐츠를 ui 내부에 바로 넣을 수 있다.
 --- 
+
+### useRef()
+입력하거나 값을 읽어올 때에 useRef의 current 등으로 쉽게 불러올 수 있다.
+>> 값이 변할 때 마다 re-rendring ? useState() : useRef()
--- meetup/src/pages/NewMeetup.js
+++ meetup/src/pages/NewMeetup.js
@@ -1,7 +1,30 @@
+import {useNavigate} from 'react-router-dom';
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
+  const history = useNavigate();
+
+  function handleMeetup(meetupData) {
+    fetch('https://udemy-react-60eb9-default-rtdb.firebaseio.com/meetups.json', {
+      //data 저장을 위해 Post
+      method: 'POST',
+      body: JSON.stringify(meetupData), //js obj to json
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      history('/', {
+        replace: true
+      }); //navigate to home page
+    });
+  }
   return (
-    <div>NewMetetup Page</div>
-  )
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={handleMeetup} />
+    </section>
+  );
 }
 
-export default NewMeetupPage
+export default NewMeetupPage;
