1c0306d2b5e2b615c539d4f25038929fa5126985
8ark <sorlti6952@gmail.com>
2023-07-18 14:02:43 +0900

    FECH: filter data
    +filter data using getServerSideProps()

--- events/pages/events/[eventId].js
+++ events/pages/events/[eventId].js
@@ -4,17 +4,17 @@ import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-import { getEventById , getAllEvents} from '../../helpers/api-util';
+import { getEventById , getAllEvents , getFeaturedEvents} from '../../helpers/api-util';
 
 
 function EventDetailPage(props) {
-  const event = props.event;
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className='center'>
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -43,17 +43,17 @@ export async function getStaticProps(context) {
 
   return {
     props: {
-      event: event,
+      selectedEvent: event,
     },
     revalidate: 30,
   }
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map(e => ({params : {eventId: e.id}}))
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   }
 }
